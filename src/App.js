c80d7ecbc392d1a4ada0d98854b66a4439d2692e
Adelaide Zhang <adelaidemzhang@gmail.com>
2023-08-18 00:37:03 -0400

    add d3 and basic test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect, useRef} from 'react';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Visualization Test - React
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Circles />
+        
       </header>
     </div>
   );
 }
 
-export default App;
+// from https://2019.wattenberger.com/blog/react-and-d3
+const Circles = () => {
+  const generateDataset = () => (
+    Array(10).fill(0).map(() => ([
+      Math.random() * 80 + 10,
+      Math.random() * 35 + 10,
+      Math.floor(Math.random()*16777215).toString(16)
+    ]))
+  )
+
+  const [dataset, setDataset] = useState(
+    generateDataset()
+  )
+
+  useInterval(() => {
+     const newDataset = generateDataset()
+      setDataset(newDataset)
+  }, 2000)
+ 
+
+  return (
+    <svg viewBox="0 0 100 50">
+      {dataset.map(([x, y, color], i) => (
+        <circle
+          key={i}
+          cx={x}
+          cy={y}
+          fill={"#" + color}
+          r="3"
+        />
+      ))}
+    </svg>
+  )
+}
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
